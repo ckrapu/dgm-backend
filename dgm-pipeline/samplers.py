6f28583b3be548f8d75321f88f5babfc678627a2
Chris Krapu <ckrapu@gmail.com>
2020-04-27 11:09:51 -0700

    Dropped old sampler code

--- dgm-pipeline/samplers.py
+++ dgm-pipeline/samplers.py
@@ -1,34 +1,79 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
-import numpy as np
 
-from functools import partial
-from utils     import parse_spec_optimizer, parse_spec_prior, parse_spec_loss
-from tqdm      import trange
 from warnings import warn
 from time     import time
 
-VERY_LARGE_ENERGY = 1e10
-DTYPE = 'float32'
+def init_kernel(target_log_prob_fn, method='nuts', step_size=0.05, ub=[]):
+    
+    if method == 'nuts':
+        inner_kernel=tfp.mcmc.NoUTurnSampler(
+            target_log_prob_fn,
+            step_size=step_size,max_tree_depth=10)
+        accept_prob = 0.8
 
-valid_priors = ['normal', 'uniform']
+        def trace_fn(_, pkr):
+            return (
+                pkr.inner_results.inner_results.target_log_prob,
+                pkr.inner_results.inner_results.leapfrogs_taken,
+                pkr.inner_results.inner_results.has_divergence,
+                pkr.inner_results.inner_results.energy,
+                pkr.inner_results.inner_results.log_accept_ratio
+                )
 
-def run_nuts_chain(init_state, step_size, target_log_prob_fn, unconstraining_bijectors,
+    elif method == 'mala':
+        inner_kernel=tfp.mcmc.MetropolisAdjustedLangevinAlgorithm(
+            target_log_prob_fn, step_size)
+        accept_prob = 0.4
+
+        def trace_fn(_, pkr):
+            pass
+
+    kernel = tfp.mcmc.TransformedTransitionKernel(inner_kernel, bijector=ub)
+
+    wrapped_kernel = tfp.mcmc.DualAveragingStepSizeAdaptation(
+        inner_kernel=kernel,
+        target_accept_prob=accept_prob,
+        num_adaptation_steps=burnin,
+        step_size_setter_fn=lambda pkr, new_step_size: pkr._replace(
+            inner_results=pkr.inner_results._replace(step_size=new_step_size)),
+        step_size_getter_fn=lambda pkr: pkr.inner_results.step_size,
+        log_accept_prob_getter_fn=lambda pkr: pkr.inner_results.log_accept_ratio
+    )
+    return wrapped_kernel, trace_fn
+
+def init_remc_kernel(target_log_prob_fn, n_chains, ub=[], leapfrog_steps=4):
+    inverse_temps = 0.5 ** tf.range(n_chains)
+    step_size = 0.5 / tf.sqrt(inverse_temperatures)
+
+    def make_kernel_fn(target_log_prob_fn, seed):
+        return tfp.mcmc.HamiltonianMonteCarlo(
+            target_log_prob_fn=target_log_prob_fn,
+            seed=seed, step_size=step_size, num_leapfrog_steps=3)
+
+    remc = tfp.mcmc.ReplicaExchangeMC(
+        target_log_prob_fn=target.log_prob,
+        inverse_temperatures=inverse_temperatures,
+        make_kernel_fn=make_kernel_fn)
+
+    def trace_fn(unused_state, results):
+        return (results.is_swap_proposed_adjacent,
+                results.is_swap_accepted_adjacent)
+
+    return remc, trace_fn
+
+
+def run_mcmc(init_state, kernel, trace_fn,
               num_steps=500, burnin=500, use_xla=True):
     '''
-    Applies MCMC using the No-U-Turn Sampler with dual step size adaptation.
+    Applies MCMC using a desired kernel function.
 
     Arguments
     ---------
     init_state : list of Numpy arrays
         Starting point for the Markov chain
-    step_size : float
-        Size of the steps to be taken by the leapfrog integrator
-    target_log_prob_fn : function
-        Tensorflow function which yields the log posterior density
-    unconstraining_bijectors : list of Bijectors
-        List of bijector functions used to map the parameters from
-        their original scale to an unconstrained scale
+    kernel : TFP TransitionKernel
+        MCMC kernel function for generating proposals
     num_steps : int
         Number of samples drawn after burn-in
     burnin : int
@@ -44,30 +89,6 @@ def run_nuts_chain(init_state, step_size, target_log_prob_fn, unconstraining_bij
         Diagnostics for NUTS performance
     '''
 
-    def trace_fn(_, pkr):
-        return (
-            pkr.inner_results.inner_results.target_log_prob,
-            pkr.inner_results.inner_results.leapfrogs_taken,
-            pkr.inner_results.inner_results.has_divergence,
-            pkr.inner_results.inner_results.energy,
-            pkr.inner_results.inner_results.log_accept_ratio
-               )
-  
-    kernel = tfp.mcmc.TransformedTransitionKernel(
-    inner_kernel=tfp.mcmc.NoUTurnSampler(
-      target_log_prob_fn,
-      step_size=step_size,max_tree_depth=10),
-    bijector=unconstraining_bijectors)
-
-    hmc = tfp.mcmc.DualAveragingStepSizeAdaptation(
-        inner_kernel=kernel,
-        num_adaptation_steps=burnin,
-        step_size_setter_fn=lambda pkr, new_step_size: pkr._replace(
-            inner_results=pkr.inner_results._replace(step_size=new_step_size)),
-        step_size_getter_fn=lambda pkr: pkr.inner_results.step_size,
-        log_accept_prob_getter_fn=lambda pkr: pkr.inner_results.log_accept_ratio
-      )
-
     # To prevent retracing that occurs when the larger function
     # is supplied non-Tensorflow arguments
     @tf.function(autograph=True, experimental_compile=use_xla)
@@ -78,7 +99,7 @@ def run_nuts_chain(init_state, step_size, target_log_prob_fn, unconstraining_bij
       num_results=num_steps,
       num_burnin_steps=burnin,
       current_state=init_state,
-      kernel=hmc,
+      kernel=kernel,
       trace_fn=trace_fn)
     total = int(time() - start)
     warn(r'{total} seconds elapsed during.',UserWarning)
